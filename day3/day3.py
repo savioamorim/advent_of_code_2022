330df9ad0ef85ab728ae9baba99ffce259b5b351
Savio Amorim <savioamorim.s@hotmail.com>
2022-12-04 10:18:32 -0300

    Refactor Day3

--- day3/day3.py
+++ day3/day3.py
@@ -16,28 +16,23 @@ with open("day3/input_day3.txt") as file_:
     letters_first_part = [[letter for letter in set(first)] for first in first_part]
     letters_second_part = [[letter for letter in set(first)] for first in second_part]
 
-    share_items = []
-    total_sum = 0
+    total_sum_priorities_items = 0
     for index, letters in enumerate(letters_first_part):
         for letter in letters:
             if letter in letters_second_part[index]:
-                share_items.append(letter)
-                total_sum += priorities[letter]
-    print(total_sum)
+                total_sum_priorities_items += priorities[letter]
+    print(f"{total_sum_priorities_items=}")
 
     # Part 2
     input_list = [item for item in input.split("\n")]
 
     x = 0
     y = 3
-    share_items = []
+    total_sum_group_of_three_intersection = 0
     for num in range(int(len(input_list)/3)):
         group_of_three = input_list[x:y]
-        share_items.append(list(set(group_of_three[0]).intersection(group_of_three[1], group_of_three[2])))
+        intersection_letter = list(set(group_of_three[0]).intersection(group_of_three[1], group_of_three[2]))
         x+=3
         y+=3
-    
-    total_sum = 0
-    for item in share_items:
-        total_sum += priorities[item[0]]
-    print(total_sum)
+        total_sum_group_of_three_intersection += priorities[intersection_letter[0]]
+    print(f"{total_sum_group_of_three_intersection=}")
