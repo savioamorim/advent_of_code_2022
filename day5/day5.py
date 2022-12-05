71c5b39fee51a7c0e713039e63ca7bdcc18d40d6
Savio Amorim <savioamorim.s@hotmail.com>
2022-12-05 12:29:55 -0300

    Refactor Day5

--- day5/day5.py
+++ day5/day5.py
@@ -10,7 +10,9 @@
  1   2   3   4   5   6   7   8   9 
 """
 
-# Part 1
+from copy import deepcopy
+
+
 stacks = {
     "1": ["J", "H", "G", "M", "Z", "N", "T", "F"],
     "2": ["V", "W", "J"],
@@ -26,17 +28,14 @@ stacks = {
 
 def move_crates(qty: int, stack_from: str, stack_to: str):
     for _ in range(qty):
-        stacks[stack_to].append(stacks[stack_from].pop())
+        stacks_part_1[stack_to].append(stacks_part_1[stack_from].pop())
 
 def move_crates_reverse(qty: int, stack_from: str, stack_to: str):
     crates_pop = []
     for _ in range(qty):
-        try:
-            crates_pop.append(stacks[stack_from].pop())
-        except:
-            print("")
+        crates_pop.append(stacks_part_2[stack_from].pop())
     crates_pop.reverse()
-    stacks[stack_to].extend(crates_pop)
+    stacks_part_2[stack_to].extend(crates_pop)
 
 def get_qty(move: str):
     qty = move.replace("move ", "").strip()
@@ -46,49 +45,29 @@ def get_stacks_position(move: str):
     stack_from, stack_to = move.split("to")
     return stack_from.strip(), stack_to.strip()
 
-
 with open("day5/input_day5.txt") as file_:
     input = file_.read()
+
+    stacks_part_1 = deepcopy(stacks)
+    stacks_part_2 = deepcopy(stacks)
 
     for move in input.split("\n"):
         move_splitted = move.split("from")
         qty = get_qty(move_splitted[0])
         stack_from, stack_to = get_stacks_position(move_splitted[1])
 
+        # Part 1
         move_crates(qty, stack_from, stack_to)
-
-    final_crates_on_top = []
-    for values in stacks.values():
-        final_crates_on_top.append(values.pop())
-
-    print("".join(final_crates_on_top))
-
-
-# Part 2
-stacks = {
-    "1": ["J", "H", "G", "M", "Z", "N", "T", "F"],
-    "2": ["V", "W", "J"],
-    "3": ["G", "V", "L", "J", "B", "T", "H"],
-    "4": ["B", "P", "J", "N", "C", "D", "V", "L"],
-    "5": ["F", "W", "S", "M", "P", "R", "G"],
-    "6": ["G", "H", "C", "F", "B", "N", "V", "M"],
-    "7": ["D", "H", "G", "M", "R"],
-    "8": ["H", "N", "M", "V", "Z", "D"],
-    "9": ["G", "N", "F", "H"],
-}
-
-with open("day5/input_day5.txt") as file_:
-    input = file_.read()
-
-    for move in input.split("\n"):
-        move_splitted = move.split("from")
-        qty = get_qty(move_splitted[0])
-        stack_from, stack_to = get_stacks_position(move_splitted[1])
-
+        # Part 2
         move_crates_reverse(qty, stack_from, stack_to)
 
-    final_crates_on_top = []
-    for values in stacks.values():
-        final_crates_on_top.append(values.pop())
+    final_crates_on_top_part_1 = []
+    for values in stacks_part_1.values():
+        final_crates_on_top_part_1.append(values.pop())
+    
+    final_crates_on_top_part_2 = []
+    for values in stacks_part_2.values():
+        final_crates_on_top_part_2.append(values.pop())
 
-    print("".join(final_crates_on_top))
+    print("Part 1 top crates:", "".join(final_crates_on_top_part_1))
+    print("Part 2 top crates:", "".join(final_crates_on_top_part_2))
